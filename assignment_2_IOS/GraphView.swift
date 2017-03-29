2dfebbe356b2593a0a6a2fa14df58d32168ef745
ivjunka <pronl@hotmail.com>
2017-03-29 04:22:06 -0700

    axes drawing added

---
+++ assignment_2_IOS/GraphView.swift
@@ -0,0 +1,58 @@
+//
+//  GraphViewController.swift
+//  assignment_2_IOS
+//
+//  Created by Home on 27/03/17.
+//  Copyright © 2017 nl.han.ica.mad. All rights reserved.
+//
+
+import UIKit
+@objc protocol GraphViewDelegate {
+    optional func scale(scale: CGFloat, sender: GraphView)
+    optional func origin(origin: CGPoint, sender: GraphView)
+}
+class GraphView: UIView {
+    @IBInspectable var axeColor: UIColor = UIColor.blueColor();
+    private let drawer = AxesDrawer();
+    
+    
+//    private var graphFrame : CGRect {
+//        return convertRect(frame, fromView: superview)
+//    }
+    
+//    let rect = CGRect(x: 0, y: 0, width: 100, height: 100);
+//    let size = CGSize(width: 100, height: 100);
+//    let point = CGPoint(x: 0, y: 0);
+    var pointsPerUnit : CGFloat = 100;
+    var delegate : GraphViewDelegate?
+    var newAxeOrigin: CGPoint?
+    private var axeOrigin : CGPoint {
+        get {
+            return newAxeOrigin ?? convertPoint(center, fromView: superview) }
+        set {
+            newAxeOrigin = newValue
+            delegate?.origin!(newValue, sender: self)
+            setNeedsDisplay()
+        }
+    }
+    
+    override func drawRect(rect: CGRect) {
+        drawer.drawAxesInRect(bounds, origin: axeOrigin, pointsPerUnit: pointsPerUnit)
+        drawPoint()
+    }
+    func drawPoint(){
+        let desiredX: CGFloat = 2
+        let desiredY: CGFloat = 2
+        let xCoord: CGFloat = (axeOrigin.x + pointsPerUnit) * desiredX
+        let yCoord: CGFloat = (axeOrigin.y + pointsPerUnit) * desiredY
+        let radius: CGFloat = 8
+        
+        let dotPath = UIBezierPath(ovalInRect: CGRectMake(xCoord, yCoord, radius, radius))
+        
+        let layer = CAShapeLayer()
+        layer.path = dotPath.CGPath
+        layer.strokeColor = UIColor.blueColor().CGColor
+        
+        self.layer.addSublayer(layer)
+    }
+}
--- assignment_2_IOS/GraphViewController.swift
+++ assignment_2_IOS/GraphViewController.swift
@@ -2,32 +2,12 @@
 //  GraphViewController.swift
 //  assignment_2_IOS
 //
-//  Created by Home on 27/03/17.
+//  Created by Home on 28/03/17.
 //  Copyright © 2017 nl.han.ica.mad. All rights reserved.
 //
 
 import UIKit
 
-class GraphViewController: UIViewController {
+class GraphViewController: UIViewController{
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
